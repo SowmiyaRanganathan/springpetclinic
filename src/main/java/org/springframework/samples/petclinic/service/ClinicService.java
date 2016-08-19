0022dc09364c9646a51e1949959944c501124f11
basservice <venkatesh.anbalagan@cognizant.com>
2016-08-19 12:22:09 +0530

    Update ClinicService.java
    Changed comment

--- src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
@@ -28,7 +28,7 @@ import org.springframework.samples.petclinic.model.Visit;
 /**
  * Mostly used as a facade so all controllers have a single point of entry
  *
- * @author Michael Isvy
+ * @author Michael Isvy test
  */
 public interface ClinicService {
 
